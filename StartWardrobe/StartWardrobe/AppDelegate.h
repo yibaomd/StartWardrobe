456edce0718bbf38bbfd9543e2878faeee20d0c8
xueye1989@live.cn <1244053432@qq.com>
2015-08-22 11:13:09 +0800

    添加工程

---
+++ StartWardrobe/StartWardrobe/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  StartWardrobe
+//
+//  Created by chengchao on 15/8/22.
+//  Copyright (c) 2015年 chengchao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ StartWardrobe/StartWardrobe/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  StartWardrobe
+//
+//  Created by chengchao on 15/8/22.
+//  Copyright (c) 2015年 chengchao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
